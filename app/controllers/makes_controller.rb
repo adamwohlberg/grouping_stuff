e635cdd7f19e88ba1789ed1d96637daec38eed54
adamzuckerberg <adam@adamzuckerberg.com>
2015-08-20 21:46:15 -0700

    Added controller code with joins. Added a makes index view with a simple table

--- app/controllers/makes_controller.rb
+++ app/controllers/makes_controller.rb
@@ -1,2 +1,13 @@
 class MakesController < ApplicationController
+
+	def index
+		@makes = Make.all
+		@cars = Car.all
+
+
+		@makes_with_at_least_two_models = Make.joins(:cars).group('makes.id').having('count(make_id)>1')
+		@makes_with_cars = Car.joins(:make).where(:make_id => @makes_with_at_least_two_models).group('cars.name').order('makes.name ASC, cars.name ASC')
+
+	end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,18 @@ Make.create! name: 'Toyota'
 Make.create! name: 'Mitsubishi'
 # 3
 Make.create! name: 'Ford'
+#4 Create a make with no cars
+Make.create! name: 'Makewithnocars'
 
 Car.create! name: 'Camry', make_id: 1
 Car.create! name: 'Prius', make_id: 1
 Car.create! name: 'Corolla', make_id: 1
+Car.create! name: 'RAV4', make_id: 1
+Car.create! name: 'RAV4', make_id: 1
+Car.create! name: 'RAV4', make_id: 1
+Car.create! name: 'RAV4', make_id: 1
+Car.create! name: 'RAV4', make_id: 1
+Car.create! name: 'RAV4', make_id: 1
 Car.create! name: 'RAV4', make_id: 1
 
 Car.create! name: 'Eclipse', make_id: 2
@@ -25,3 +33,13 @@ Car.create! name: 'Outlander', make_id: 2
 Car.create! name: 'Lancer', make_id: 2
 
 Car.create! name: 'Mustang', make_id: 3
+
+# Create cars with no make
+Car.create! name: 'Car1_no_nake', make_id: nil
+Car.create! name: 'Car2_no_nake', make_id: nil
+Car.create! name: 'Car3_no_nake', make_id: nil
+Car.create! name: 'Car4_no_nake', make_id: nil
+Car.create! name: 'Car5_no_nake', make_id: nil
+
+
+
